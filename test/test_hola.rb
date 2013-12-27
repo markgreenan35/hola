ab0f0eab5fdc3fe74794159d0a2ec6889bc7502b
markgreenan35 <markgreenan35@gmail.com>
2013-12-27 16:19:30 +0900

    Update test_hola.rb

--- test/test_hola.rb
+++ test/test_hola.rb
@@ -1,16 +1,16 @@
 require 'test/unit'
-require 'hola'
+require 'hola_test'
 
 class HolaTest < Test::Unit::TestCase
   def test_english_hello
-    assert_equal "hello world", Hola.hi("english")
+    assert_equal "hello world", Hola_test.hi("english")
   end
 
   def test_any_hello
-    assert_equal "hello world", Hola.hi("ruby")
+    assert_equal "hello world", Hola_test.hi("ruby")
   end
 
   def test_spanish_hello
-    assert_equal "hola mundo", Hola.hi("spanish")
+    assert_equal "hola mundo", Hola_test.hi("spanish")
   end
 end
